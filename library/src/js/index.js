926e28bf436feaa58011ac4913b5584e91ddce92
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-07-11 17:44:07 +0300

    feat: add about section

--- library/src/js/index.js
+++ library/src/js/index.js
@@ -1,3 +1,14 @@
 import '../index.html'
 import '../styles/components/header.scss'
 import '../styles/components/welcome.scss'
+import '../styles/components/about.scss'
+
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+  });
+})
