d1fde95518048a411a8ddd753fa559ef2abe9af8
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-07-14 15:19:44 +0300

    feat: add Digital card and start mobile version

--- library/src/js/slider.js
+++ library/src/js/slider.js
@@ -6,4 +6,20 @@ $('.slider').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   arrows: false,
+  varibleWidth: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      }
+
+    }
+  ]
 });
