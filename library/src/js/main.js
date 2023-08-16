da55648b43a56d8796ff5ae7b0ef7b95b5595bf9
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-08-16 22:09:31 +0300

    feat: add slider

--- library/src/js/main.js
+++ library/src/js/main.js
@@ -33,13 +33,86 @@ itemLink.forEach(item => {
   item.addEventListener('click', burgerMenuClose)
 })
 
-const profile = document.querySelector('.profile__logo')
-const profileModal = document.querySelector('.profile__modal')
+const labelsSeason = document.querySelectorAll('.season__label')
+const winter = document.querySelector('.winter')
+const spring = document.querySelector('.spring')
+const summer = document.querySelector('.summer')
+const autumn = document.querySelector('.autumn')
 
-function profileActive() {
-  profile.addEventListener('click', () => {
-    profile.classList.add('.profile__modal.active')
+const fadeIn = (el, timeout, display) => {
+  el.style.opacity = 0;
+  el.style.display = display || 'block';
+  el.style.transition = `opacity ${timeout}ms`;
+  setTimeout(() => {
+    el.style.opacity = 1;
+  }, 10)
+};
+
+const fadeOut = (el, timeout) => {
+  el.style.opacity = 1;
+  el.style.transition = `opacity ${timeout}ms`;
+  el.style.opacity = 0;
+  setTimeout(() => {
+    el.style.display = 'none'
+  }, timeout);
+};
+
+let flag = false;
+
+function seasonSort() {
+  labelsSeason[0].addEventListener('click', () => {
+    if (!flag) {
+      fadeIn(winter, 3000, 'flex');
+      flag = true;
+    } else {
+      fadeOut(spring, 500);
+      flag = false;
+      fadeOut(summer, 500);
+      flag = false;
+      fadeOut(autumn, 500);
+      flag = false;
+
+    }
+  })
+  labelsSeason[1].addEventListener('click', () => {
+    if (!flag) {
+      fadeIn(spring, 3000, 'flex');
+      flag = true;
+    } else {
+      fadeOut(winter, 500);
+      flag = false;
+      fadeOut(summer, 500);
+      flag = false;
+      fadeOut(autumn, 500);
+      flag = false;
+    }
+  })
+  labelsSeason[2].addEventListener('click', () => {
+    if (!flag) {
+      fadeIn(summer, 3000, 'flex');
+      flag = true;
+    } else {
+      fadeOut(winter, 500);
+      flag = false;
+      fadeOut(spring, 500);
+      flag = false;
+      fadeOut(autumn, 500);
+      flag = false;
+    }
+  })
+  labelsSeason[3].addEventListener('click', () => {
+    if (!flag) {
+      fadeIn(autumn, 3000, 'flex');
+      flag = true;
+    } else {
+      fadeOut(winter, 500);
+      flag = false;
+      fadeOut(spring, 500);
+      flag = false;
+      fadeOut(summer, 500);
+      flag = false;
+    }
   })
 }
 
-profileActive()
+seasonSort()
--- library/src/js/slider.js
+++ library/src/js/slider.js
@@ -1,27 +1,111 @@
-import $ from 'jquery'
-import 'slick-slider'
+const slider = document.querySelector('.slider');
+const sliderTrack = document.querySelector('.slider__track');
+const buttonPrev = document.querySelector('.prev');
+const buttonNext = document.querySelector('.next');
+const buttonsContainer = document.querySelector('.slider__buttons');
+const picAll = slider.querySelectorAll('.pic');
 
-$('.slider').slick({
-  dots: true,
-  infinite: false,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  arrows: false,
-  prevArrow: $('.prev'),
-  nextArrow: $('.next'),
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-      }
-    },
-    {
-      breakpoint: 769,
-      settings: {
-        slidesToShow: 1,
-        arrows: true
-      }
+let shift = 0;
+let picWidth;
+const activePicCount = 1;
+let sliderWidth = slider.clientWidth;
+
+function resizeWidth() {
+  let windowWidth = window.innerWidth;
+  sliderWidth = slider.clientWidth;
+
+  if (windowWidth > 1024) {
+    picWidth = sliderWidth / 3 - (50 / 3);
+  }
+  if (windowWidth <= 1024) {
+    picWidth = sliderWidth / 2;
+  }
+  if (windowWidth <= 768) {
+    slider.style.maxWidth = 450 + 'px';
+    picWidth = sliderWidth;
+  }
+  if (windowWidth > 768) {
+    slider.style.maxWidth = 1400 + 'px';
+  }
+  picAll.forEach(item => item.style.width = picWidth + 'px');
+  addButtonsSlideControl(sliderWidth)
+}
+
+resizeWidth()
+
+function addButtonsSlideControl(sliderWidth) {
+  let buttonsCount = picAll.length - (Math.floor(sliderWidth / picWidth)) + activePicCount;
+  const buttonSlideContolHTML = '<span class="slider__button"></span>'
+
+  buttonsContainer.innerHTML = '';
+
+  for (let i = 0; i < buttonsCount; i++) {
+    buttonsContainer.insertAdjacentHTML('beforeend', buttonSlideContolHTML)
+  }
+  const buttonsCircle = document.querySelectorAll('.slider__button');
+
+  buttonsCircle.forEach((button, index) => {
+    if (index === 0) {
+      button.classList.add('button__active')
     }
-  ]
-});
+
+    button.addEventListener('click', () => {
+
+      buttonsCircle.forEach(button => {
+        button.classList.remove('button__active')
+      })
+
+      if (index === 0) {
+        shift = 0;
+        button.classList.add('button__active')
+      }
+      if (shift < picWidth * 4 + 100) {
+        shift = (picWidth + 25) * index;
+      }
+      sliderTrack.style.transform = `translateX(-${shift}px)`;
+      button.classList.add('button__active')
+    })
+  })
+}
+
+addButtonsSlideControl(sliderWidth)
+
+function disabledButtons() {
+  if (shift === 0) {
+    buttonPrev.classList.add('arrow__disabled');
+  } else {
+    buttonPrev.classList.remove('arrow__disabled');
+  }
+  if (shift === picWidth * 4 + 100) {
+    buttonNext.classList.add('arrow__disabled');
+  } else {
+    buttonNext.classList.remove('arrow__disabled');
+  }
+}
+
+buttonNext.addEventListener('click', () => {
+  if (shift === picWidth * 4 + 100) {
+    return;
+  }
+  shift += picWidth + 25;
+  sliderTrack.style.transform = `translateX(-${shift}px)`;
+  disabledButtons();
+})
+
+buttonPrev.addEventListener('click', () => {
+  if (shift === 0) {
+    shift = 0;
+    return;
+  }
+  shift -= picWidth + 25;
+  sliderTrack.style.transform = `translateX(-${shift}px)`;
+  disabledButtons();
+})
+
+disabledButtons()
+
+window.addEventListener('resize', () => {
+  resizeWidth();
+  sliderTrack.style.transform = 'translateX(0px)';
+  shift = 0;
+})
