ccca2a81921a9f0e92f41c05a0eb7a37649a4976
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-08-17 14:01:20 +0300

    fead: add register and login modals

--- library/src/js/main.js
+++ library/src/js/main.js
@@ -1,3 +1,4 @@
+// burger menu
 const burger = document.querySelector('.burger')
 const nav = document.querySelector('.nav')
 const overlay = document.querySelector('.overlay')
@@ -13,6 +14,7 @@ function burgerActive() {
     profileBurger.style.position = 'fixed'
     profileBurger.style.top = '31px'
     profileBurger.style.right = '17px'
+    profileBurger.style.zIndex = '2'
   })
 }
 
@@ -25,6 +27,7 @@ function burgerMenuClose() {
   profileBurger.style.position = 'static'
   profileBurger.style.top = '0'
   profileBurger.style.right = '0'
+  profileBurger.style.zIndex = '0'
 }
 
 overlay.addEventListener('click', burgerMenuClose)
@@ -32,6 +35,10 @@ burgerClose.addEventListener('click', burgerMenuClose)
 itemLink.forEach(item => {
   item.addEventListener('click', burgerMenuClose)
 })
+
+// burger menu
+
+// favorite cards cort
 
 const labelsSeason = document.querySelectorAll('.season__label')
 const winter = document.querySelector('.winter')
@@ -116,3 +123,76 @@ function seasonSort() {
 }
 
 seasonSort()
+
+// favorite cards cort
+
+// profile modal
+const profileModal = document.querySelector('.profile__modal');
+const profileModalBefore = document.querySelector('.profile__before');
+const profileModalAfter = document.querySelector('.profile__after');
+
+function profileModalActive() {
+  // if () {
+  // profileModalAfter.toggle('profile__modal__active');
+  // } else {
+  profileModal.classList.toggle('profile__modal__active');
+}
+
+function closeMenuOnClickOutside(event) {
+  const target = event.target;
+  if (!profileModal.contains(target) && target !== profileBurger) {
+    profileModal.classList.remove('profile__modal__active');
+  }
+}
+
+function addClickOutsideListener() {
+  document.addEventListener('click', closeMenuOnClickOutside);
+}
+
+profileBurger.addEventListener('click', profileModalActive);
+profileBurger.addEventListener('click', burgerMenuClose);
+addClickOutsideListener();
+
+// profile modal
+
+// register/login modal
+
+const registerButton = document.querySelectorAll('.register__button');
+const registerModal = document.querySelector('.register__modal');
+const registerCloseLogo = document.querySelector('.register__close');
+
+const logInButton = document.querySelectorAll('.login__button');
+const logInModal = document.querySelector('.login__modal');
+const logInCloseLogo = document.querySelector('.login__close');
+
+function ModalOpen(modalType) {
+  modalType.classList.add('modal__active');
+  overlay.classList.add('overlay__active');
+  profileModal.classList.remove('profile__modal__active');
+  if (modalType === registerModal) {
+    logInModal.classList.remove('modal__active')
+  }
+  if (modalType === logInModal) {
+    registerModal.classList.remove('modal__active')
+  }
+}
+
+function modalClose(modalType) {
+  modalType.classList.remove('modal__active');
+  overlay.classList.remove('overlay__active');
+}
+
+registerButton.forEach(item => {
+  item.addEventListener('click', () => ModalOpen(registerModal));
+});
+overlay.addEventListener('click', () => modalClose(registerModal));
+registerCloseLogo.addEventListener('click', () => modalClose(registerModal));
+
+logInButton.forEach(item => {
+  item.addEventListener('click', () => ModalOpen(logInModal));
+});
+overlay.addEventListener('click', () => modalClose(logInModal));
+logInCloseLogo.addEventListener('click', () => modalClose(logInModal));
+
+// register/login modal
+
