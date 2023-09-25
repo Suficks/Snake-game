e649ccabea7419a313c01bb4d1092ed112291c38
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-09-25 18:48:10 +0300

    feat: add like pic functional

--- image-galery/index.js
+++ image-galery/index.js
@@ -7,30 +7,33 @@ const thirdColumn = document.querySelector('.third');
 const reset = document.querySelector('.reset');
 
 let url = 'https://api.unsplash.com/search/photos?query=random&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+input.focus();
 
 async function getData() {
   const res = await fetch(url);
   const data = await res.json();
   if (!res.ok) throw new Error(response.statusText);
   showData(data)
-    .catch(error => {
-      console.error('Ошибка:', error);
-    });
 };
 
 getData();
 
-async function showData(data) {
+function showData(data) {
   firstColumn.innerHTML = '';
   secondColumn.innerHTML = '';
   thirdColumn.innerHTML = '';
 
   data.results.forEach((element, index) => {
     const pic = element.urls.regular;
-    const img = `<img class="gallery__img" src='${pic}' alt="image">`;
+    const img = `
+    <div class="img__item">
+      <img class="gallery__img" src='${pic}' alt="image">
+      <img class="like" src="./assets/like-icon.png" alt="like">
+    </div>`;
     if (index < 10) firstColumn.insertAdjacentHTML('beforeend', img);
     if (index >= 10 && index < 20) secondColumn.insertAdjacentHTML('beforeend', img);
     if (index >= 20) thirdColumn.insertAdjacentHTML('beforeend', img);
+    likePic();
   });
 };
 
@@ -56,3 +59,22 @@ reset.addEventListener('click', () => {
   input.value = '';
   resetInput();
 });
+
+function likePic() {
+  const likeBtn = document.querySelectorAll('.like');
+  let isActive = false;
+
+  likeBtn.forEach(item => {
+    item.addEventListener('click', () => {
+      if (!isActive) {
+        item.classList.add('like__active');
+        item.src = './assets/like-icon-active.png';
+        isActive = true;
+      } else {
+        item.classList.remove('like__active');
+        item.src = './assets/like-icon.png';
+        isActive = false;
+      }
+    })
+  })
+}
