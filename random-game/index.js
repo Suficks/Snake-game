6feeacee2bbbeeab144d41f8be515762c922c798
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-10-03 13:08:05 +0300

    feat: add tail to snake

--- random-game/index.js
+++ random-game/index.js
@@ -16,8 +16,8 @@ foodImg.src = foodPics[(Math.floor(Math.random() * 3))];
 const snakeHadeImg = new Image();
 snakeHadeImg.src = './assets/icon-snake-head.png';
 
-const snakeTailImg = new Image();
-snakeTailImg.src = './assets/icon-snake-tail.png';
+const snakeTaiImg = new Image();
+snakeTaiImg.src = './assets/tail.jpg';
 
 snake[0] = {
   x: 9 * box,
@@ -30,7 +30,6 @@ let food = {
 }
 
 const snakeDirection = (e) => {
-  console.log(dir);
   if (e.keyCode === 37 && dir !== 'right') dir = 'left';
   else if (e.keyCode === 38 && dir !== 'down') dir = 'up';
   else if (e.keyCode === 39 && dir !== 'left') dir = 'right';
@@ -44,24 +43,29 @@ const drawGame = () => {
   ctx.drawImage(foodImg, food.x, food.y);
 
   for (let i = 0; i < snake.length; i++) {
-    if (i === 0) ctx.drawImage(snakeHadeImg, snake[i].x, snake[i].y);
-    else ctx.drawImage(snakeTailImg, snake[i].x, snake[i].y);
+    if (i === 0) ctx.drawImage(snakeHadeImg, snake[i].x, snake[i].y)
+    else ctx.drawImage(snakeTaiImg, snake[i].x, snake[i].y)
   }
+
   ctx.fillStyle = 'white';
-  ctx.font = '45px Arial';
-  ctx.fillText(score, box * 2, box * 1.7)
+  ctx.font = '40px Arial';
+  ctx.fillText(score, box * 2, box * 1.6)
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-
   if (snakeX === food.x && snakeY === food.y) {
     score++;
+    foodImg.src = foodPics[(Math.floor(Math.random() * 3))];
+    ctx.drawImage(foodImg, food.x, food.y);
     food = {
       x: (Math.floor(Math.random() * 17 + 1)) * box,
       y: (Math.floor(Math.random() * 15 + 3)) * box,
     }
   } else snake.pop();
+
+  if (snakeX < box || snakeX > box * 17
+    || snakeY < 3 * box || snakeY > box * 17) game.clearInterval();
 
   if (dir === 'left') snakeX -= box;
   if (dir === 'right') snakeX += box;
